4799c1aee63c64c99d58df20566eebe7cddfd8ba
Juan Pablo Arango <juanpabloarango.iearm@gmail.com>
2024-04-03 06:43:14 -0500

    Prueba

---
+++ src/app/components/editarperfil/editarperfil..ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from "@angular/router"; 
+import { Page, TextField } from '@nativescript/core';
+
+@Component({
+  selector: 'editarperfil',
+  templateUrl: './editarperfil.html',
+  styleUrls: ['./editarperfil.css']
+
+  
+})
+export class PerfilComponent {
+  public constructor(private router: Router, private page: Page) {
+    // Use the component constructor to inject providers.
+  }
+  ngOnInit(): void {
+    this.page.actionBarHidden = true;
+  }
+  public onTap(){
+    this.router.navigate(["home"])}
+}
