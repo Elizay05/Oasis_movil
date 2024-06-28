437552ef3a5e7615bdd3079e93905f983163e987
Sneyder Vergara Bolivar <sneyder_vergara@soy.sena.edu.co>
2024-06-28 11:42:41 -0500

    ...

--- src/app/components/perfil/perfil.ts
+++ src/app/components/perfil/perfil.ts
@@ -19,6 +19,21 @@ export class PerfilComponent {
   email: string = '';
   public constructor(private router: Router, private page: Page,) {
     // Use the component constructor to inject providers.
+    if (localStorage.getItem('Oasis.token')) {
+      this.perfil = JSON.parse(localStorage.getItem('Oasis.user'))
+      console.log("Bienvenido " + this.perfil.nombre + "!!");
+      this.rol = this.perfil.rol
+      this.nombre = this.perfil.nombre
+      this.foto = global.url+this.perfil.foto
+      this.email = this.perfil.email
+    }
+    else {
+      this.rol = ""
+      this.nombre = ""
+      this.foto = ""
+      this.email = ""
+      this.router.navigate(['login']);
+    }
   }
   ngOnInit(): void {
     this.page.actionBarHidden = true;
